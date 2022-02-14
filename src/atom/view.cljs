3bb31a3ffaee620da693c0183d3529f392ed3951
Maurício Szabo <mauricio.szabo@gmail.com>
2022-02-13 23:50:42 -0300

    Removed the "snapshot", whatever that was

--- src/atom/view.cljs
+++ src/atom/view.cljs
@@ -11,13 +11,10 @@
 
 (defn ^js getWindowLoadSettings []
   (get-load-fn))
-  ; ((js/require "../src/get-window-load-settings")))
 
 (def entryPointDirPath js/__dirname)
 
 (def blobStore nil)
-
-(def useSnapshot false)
 
 (def startupMarkers
   (.-startupMarkers (.getCurrentWindow (.-remote electron))))
@@ -31,8 +28,7 @@
   (when (.-atom js/global)
     (aset (.-atom js/global) "loadTime" loadTime)))
 
-(defn handleSetupError
-  [error]
+(defn handleSetupError [error]
   (let [currentWindow (.getCurrentWindow (.-remote electron))]
     (.setSize currentWindow 800 600)
     (.center currentWindow)
@@ -40,12 +36,11 @@
     (.openDevTools currentWindow)
     (.error js/console (or (.-stack error) error))))
 
-(defn setupAtomHome
-  []
-  (when (.-ATOM_HOME (.-env js/process)) (js* "return"))
-  (when-let [home  (and (getWindowLoadSettings)
-                        (.-atomHome (getWindowLoadSettings)))]
-    (set! (.. js/process -env -ATOM_HOME) home)))
+(defn setupAtomHome []
+  (when-not (.-ATOM_HOME (.-env js/process))
+    (when-let [home (and (getWindowLoadSettings)
+                         (.-atomHome (getWindowLoadSettings)))]
+      (set! (.. js/process -env -ATOM_HOME) home))))
 
 (def ^:private initialize
   (delay
@@ -54,10 +49,7 @@
                            entryPointDirPath
                            (.-windowInitializationScript
                             (getWindowLoadSettings)))]
-     (if useSnapshot
-       (.customRequire js/snapshotResult
-                       init-script-path)
-       (js/require init-script-path)))))
+     (js/require init-script-path))))
 
 (defn init-editor []
   (p/do!
@@ -65,21 +57,11 @@
    (.addMarker startup-time "window:initialize:end")
    (.send (.-ipcRenderer electron) "window-command" "window:loaded")))
 
-(defn setupWindow
-  []
-  (let [CompileCache (if useSnapshot
-                       (.customRequire js/snapshotResult "../src/compile-cache.js")
-                       (js/require "../src/compile-cache"))
-        ModuleCache (if useSnapshot
-                      (.customRequire js/snapshotResult "../src/module-cache.js")
-                      (js/require "../src/module-cache"))
-        startCrashReporter
-        (if useSnapshot
-          (.customRequire js/snapshotResult "../src/crash-reporter-start.js")
-          (js/require "../src/crash-reporter-start"))
-        Grim (if useSnapshot
-               (.customRequire js/snapshotResult "../node_modules/grim/lib/grim.js")
-               (js/require "grim"))
+(defn setupWindow []
+  (let [CompileCache (js/require "../src/compile-cache")
+        ModuleCache (js/require "../src/module-cache")
+        startCrashReporter (js/require "../src/crash-reporter-start")
+        Grim (js/require "grim")
         documentRegisterElement (.-registerElement js/document)
         --temp1 (getWindowLoadSettings)
         userSettings (.-userSettings --temp1)
@@ -88,22 +70,14 @@
                             (.-core userSettings)
                             (= (.-telemetryConsent (.-core userSettings)) "limited"))
         releaseChannel (get-release-channel "9.4.4")
-        CSON (if useSnapshot
-               (.customRequire
-                js/snapshotResult
-                "../node_modules/season/lib/cson.js")
-               (js/require "season"))]
+        CSON (js/require "season")]
     (.setAtomHomeDirectory CompileCache
                            (.-ATOM_HOME (.-env js/process)))
     (.install CompileCache
               (.-resourcesPath js/process)
               js/require)
     (.register ModuleCache (getWindowLoadSettings))
-    (if useSnapshot
-      (.customRequire
-       js/snapshotResult
-       "../node_modules/document-register-element/build/document-register-element.node.js")
-      (js/require "document-register-element"))
+    (js/require "document-register-element")
     (aset
      js/document
      "registerElement"
@@ -155,18 +129,8 @@
                                (.-resourcePath (getWindowLoadSettings))
                                (+ (.-resourcesPath js/process)
                                   (.-sep path)))))
-             FileSystemBlobStore
-             (if useSnapshot
-               (.customRequire
-                js/snapshotResult
-                "../src/file-system-blob-store.js")
-               (js/require "../src/file-system-blob-store"))
-             NativeCompileCache
-             (if useSnapshot
-               (.customRequire
-                js/snapshotResult
-                "../src/native-compile-cache.js")
-               (js/require "../src/native-compile-cache"))]
+             FileSystemBlobStore (js/require "../src/file-system-blob-store")
+             NativeCompileCache (js/require "../src/native-compile-cache")]
          (.addMarker startup-time "window:onload:start")
          (.on
           js/process
@@ -181,9 +145,7 @@
                "resourcesPath"
                (.normalize path (.-resourcesPath js/process)))
          (setupAtomHome)
-         (set! useSnapshot
-           (and (not devMode)
-                (not (undefined? js/snapshotResult))))
+         ;; FIXME - devmode?
          (if devMode
            (let [metadata (js/require "../package.json")]
              (when (not (.-_deprecatedPackages metadata))
@@ -196,49 +158,7 @@
                    (.error
                     js/console
                     "Failed to setup deprecated packages list"
-                    (.-stack requireError))))))
-           (when useSnapshot
-             (do (aset
-                  (.-prototype Module)
-                  "require"
-                  (fn [module]
-                    (let [absoluteFilePath (._resolveFilename
-                                            Module
-                                            module
-                                            (js* "this")
-                                            false)
-                          relativeFilePath (.relative
-                                            path
-                                            entryPointDirPath
-                                            absoluteFilePath)
-                          cachedModule
-                          (aget (.-cache (.-customRequire
-                                          js/snapshotResult))
-                                relativeFilePath)]
-                      (when (= (.-platform js/process) "win32")
-                        (def relativeFilePath
-                          (.replace relativeFilePath
-                                    (js* "/\\\\/g")
-                                    "/")))
-                      (when (not cachedModule)
-                        (def cachedModule
-                          #js
-                          {:exports (._load Module
-                                            module
-                                            (js* "this")
-                                            false)})
-                        (aset (.-cache (.-customRequire js/snapshotResult))
-                              relativeFilePath
-                              cachedModule))
-                      (.-exports cachedModule))))
-               (.setGlobals js/snapshotResult
-                            js/global
-                            js/process
-                            js/window
-                            js/document
-                            js/console
-                            js/require))))
-         (def FileSystemBlobStore FileSystemBlobStore)
+                    (.-stack requireError)))))))
          (set! blobStore
            (.load FileSystemBlobStore
                   (.join path
